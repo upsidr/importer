f059bed310af18681d107d2e52bb2629cbbff148
Ryota <rytswd@gmail.com>
2021-08-12 17:58:22 +0100

    Use proper replace logic

--- internal/file/replace.go
+++ internal/file/replace.go
@@ -26,71 +26,42 @@ func WithForce() ReplaceOption {
 }
 
 // ReplaceWithAfter replaces the original file content with the processed
-// content. This is done by creating a temp file first, and replacing it.
-//
-// TODO: Ensure file mode is kept, or clarify in the comment.
+// content.
 func (f *File) ReplaceWithAfter(options ...ReplaceOption) error {
 	mode := &replaceMode{}
 	for _, opt := range options {
 		opt(mode)
 	}
 
-	file, err := os.CreateTemp("/tmp/", "importer_replace_*")
-	if err != nil {
-		return err // TODO: test coverage
-	}
-	defer file.Close()
-
-	_, err = file.Write(f.ContentAfter)
-	if err != nil {
-		return err // TODO: test coverage
-	}
-
-	// Exit early if dry-run
-	if mode.isDryRun {
-		return nil
-	}
-
-	err = os.Rename(file.Name(), f.FileName)
-	if err != nil {
-		return err // TODO: test coverage
-	}
-
-	return nil
+	return replace(f.FileName, f.ContentAfter, mode)
 }
 
 // ReplaceWithAfter replaces the original file content with the processed
-// content. This is done by creating a temp file first, and replacing it.
-//
-// TODO: Ensure file mode is kept, or clarify in the comment.
+// content.
 func (f *File) ReplaceWithPurged(options ...ReplaceOption) error {
 	mode := &replaceMode{}
 	for _, opt := range options {
 		opt(mode)
 	}
 
-	file, err := os.CreateTemp("/tmp/", "importer_replace_*")
-	if err != nil {
-		return err // TODO: test coverage
-	}
-	defer file.Close()
-
 	data := strings.Join(f.ContentPurged, "\n")
 	data = data + "\n" // Make sure to add new line at the end of the file
-	_, err = file.WriteString(data)
-	if err != nil {
-		return err // TODO: test coverage
-	}
 
+	return replace(f.FileName, []byte(data), mode)
+}
+
+func replace(fileName string, content []byte, mode *replaceMode) error {
 	// Exit early if dry-run
 	if mode.isDryRun {
 		return nil
 	}
 
-	err = os.Rename(file.Name(), f.FileName)
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		return err // TODO: test coverage
+		return err
 	}
+	defer file.Close()
 
-	return nil
+	_, err = file.Write(content)
+	return err
 }
