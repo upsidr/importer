2e49f20852b625a516e3778479bc3a4643d60557
Ryota <rytswd@gmail.com>
2021-09-03 22:23:09 +0100

    Remove duplicated message

--- internal/marker/errors.go
+++ internal/marker/errors.go
@@ -7,7 +7,7 @@ var (
 	ErrNoInput             = errors.New("no file content found")
 	ErrInvalidPath         = errors.New("invalid path provided")
 	ErrInvalidSyntax       = errors.New("invalid syntax given")
-	ErrNoMatchingMarker    = errors.New("no matching marker found, marker must be a begin/end pair")
+	ErrNoMatchingMarker    = errors.New("no matching marker found")
 	ErrMissingOption       = errors.New("missing option")
 	ErrMissingName         = errors.New("unnamed marker cannot be used")
 	ErrNoFileInput         = errors.New("no target file input provided")
