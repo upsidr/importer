3cb3e4700cf46f428b816b9f52c09c6146746b56
Ryota <rytswd@gmail.com>
2022-01-05 18:18:45 -0500

    Remove static file and use goreleaser

--- tools/prep-release.sh
+++ tools/prep-release.sh
@@ -5,5 +5,10 @@ __root_dir=$(dirname "$0")/..
 # Fill in revision information
 git rev-parse --short HEAD >"$__root_dir"/internal/version/REVISION.txt
 
-# Copy the version info in the root dir
-cp "$__root_dir"/VERSION.txt "$__root_dir"/internal/version/VERSION.txt
+# Assuming that tag value is provided as an argument, use that as the version
+# information. If missing by any chance, this defaults to "unknown".
+__tag=$1
+if [[ -z $__tag ]]; then
+    __tag="unknown"
+fi
+echo "$__tag" >"$__root_dir"/internal/version/VERSION.txt
